99818ed7b40dff1f2ebae3ba1b5547e15f546638
Fuad Ariqoh <fuadariqoh@gmail.com>
2023-06-13 19:18:55 +0700

    intial commit

---
+++ src/entities/account.entity.ts
@@ -0,0 +1,6 @@
+export type Account = {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+};
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <>
+      <h5>This is login</h5>
+    </>
+  );
+}
